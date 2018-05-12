4b1517ef8a4143dbf88b6d80e121d4e689ebb51d
Carlos Flores <carlosjf115@gmail.com>
2018-05-11 22:59:56 -0600

    segundo commit cliente

--- Cliente.java
+++ Cliente.java
@@ -5,5 +5,16 @@ public class Cliente {
     private int Idcliente;
     private String nombrecliente;
     private String nit;
+
+    public Cliente() {
+    }
+
+    public Cliente(int Idcliente, String nombrecliente, String nit) {
+        this.Idcliente = Idcliente;
+        this.nombrecliente = nombrecliente;
+        this.nit = nit;
+    }
+    
+    
     
 }
